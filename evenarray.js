20b2e590faa0d75a2311a0bda1f6facc7e18ebe4
Aurelie Gedeon <aureliegedeon@Aurelies-Macbook-Pro.local>
2022-01-22 10:35:44 -0500

    added more functions

--- evenarray.js
+++ evenarray.js
@@ -9,3 +9,30 @@ function addEven(arr) {
   console.log(counter);
 }
 addEven(evenArray);
+
+//Or
+
+function addUpEvens(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sum += arr[i];
+    }
+  }
+  return sum;
+}
+console.log(addUpEvens(evenArray));
+
+//OR
+
+function filterJustEvens(arr) {
+  let newArr = [];
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      newArr.push(arr[i]);
+    }
+  }
+  return sum;
+}
+console.log(filterJustEvens(evenArray));
